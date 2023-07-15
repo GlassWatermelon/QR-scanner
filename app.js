148bf9f23abb9490c8e97bc4510c86b7499dd2ba
Mona Lisa <soriaagustin8@gmail.com>
2023-07-15 07:43:01 -0300

    Adding styles and basic functionalities

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+const $staffSection = document.querySelector('.container-security');
+const $btn = document.querySelector('.security-btn');
+const $borderBox = document.querySelector('.border-box');
+const $inputStaff = document.querySelector('.password');
+const $qrContainer = document.querySelector('.qr-scanner-container');
+
 const scanner = new Html5QrcodeScanner('reader', {
     qrbox: {
             width: 250,
@@ -10,8 +16,8 @@ scanner.render(success, error);
 
 function success(result) {
     document.getElementById('result').innerHTML = `
-        <h2>Success!</h2>
-        <p><a href="${result}">${result}</a></p>
+        <h2>Exito!</h2>
+        <p>${result}</p>
     `;
 
     scanner.clear();
@@ -22,3 +28,16 @@ function success(result) {
 function error(err) {
 console.error(err);
 }
+
+$btn.addEventListener('click', () => {
+    const $sonReaderContainer = $qrContainer.querySelector('#reader'); 
+
+    if($inputStaff.value === 'ANAWID'){
+        $staffSection.style.display = 'none';
+        $qrContainer.style.display = 'block';
+        $borderBox.style.display = 'none';
+        console.log($sonReaderContainer);
+    } else {
+        console.log('Constraseña incorrecta');
+    }
+});
